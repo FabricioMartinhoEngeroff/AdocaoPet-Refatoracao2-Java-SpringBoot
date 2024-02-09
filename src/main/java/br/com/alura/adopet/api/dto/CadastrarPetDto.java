71a2f3e8bba3ec8bae40136ef389abe0ab9cb275
Fabricio <fa.engeroff1996@gmail.com>
2024-02-09 07:58:11 -0300

    Refatoração nas classes referentes ao Abrigo

--- src/main/java/br/com/alura/adopet/api/dto/CadastrarPetDto.java
+++ src/main/java/br/com/alura/adopet/api/dto/CadastrarPetDto.java
@@ -1,4 +1,4 @@
 package br.com.alura.adopet.api.dto;
 
-public record CadastrarPetDto(Long idAbrigo, String name, String Telefone, String email) {
+public record CadastrarPetDto(Long idAdoao, String name, String Telefone, String email) {
 }
